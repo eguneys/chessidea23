5fd64194dfeaf12c33b001c96acc704247c69ab0
Emre Guneyler <eguneys@gmail.com>
2022-11-09 01:17:59 +0300

    replay

--- src/idea_replay_ctrl.ts
+++ src/idea_replay_ctrl.ts
@@ -1,30 +1,138 @@
 import { Signal, createMemo, createSignal } from 'solid-js'
-import { vec2_poss, Vec2, read, write, owrite, DragEvent, EventPosition, Memo, Ref, make_drag_from_ref } from 'solid-play'
-import { Board, MobileSituation, initial_fen } from 'lchessanalysis'
+import { m_log, vec2_poss, Vec2, read, write, owrite, DragEvent, EventPosition, Memo, Ref, make_drag_from_ref, make_wheel_from_ref } from 'solid-play'
+import { Replay, Board, MobileSituation, initial_fen } from 'lchessanalysis'
+
+export type OD = string
+function playMoves(situation: MobileSituation, moves: Array<OD>): MobileSituation {
+  let move = moves.shift()
+
+  if (move) {
+    return playMoves(situation.od(move)[0], moves)
+  }
+  return situation
+}
+
+const make_replay_fen = (moves: string) => {
+  let __ = moves.split('\n')
+  return __.map(_ => {
+    let [path, data] = _.split('_separator_')
+    let [,_data] = data.match(/\{(.*)\}/)!
+    let [_uci] = _data.split(' ')
+
+    return [path, _uci].join(' ')
+  })
+}
+
 
 export class _Chessideareplay23 {
 
-  on_move(_: string) {
+  on_click(path: string) {
+    owrite(this._path, path)
+  }
+
+  on_move(od: string) {
+    let path
+    owrite(this._replay, _ => {
+      let _path = read(this._path)
+      if (_) {
+        if (_path) {
+          path = _.play_ucis(_path, od)
+        } else {
+          let res = Replay.from_fen(od)
+          path = res.root.path
+          return res
+        }
+        return _
+      } else {
+        let res = Replay.from_fen(od)
+        path = res.root.path
+        return res
+      }
+    })
+    if (path) {
+      owrite(this._path, path)
+    }
+  }
+
+  get path() {
+    return read(this._path)
   }
 
   get fen() {
-    return initial_fen
+    return this.m_situation().fen
   }
 
 
   get allowed_ods() {
-    return ""
+    return this.m_situation().ods.join(' ')
   }
 
   get replay_moves() {
+    let replay = read(this._replay)?.replay
+
+    if (replay) {
+      return make_replay_fen(replay)
+    }
     return []
   }
 
   onScroll() {
   }
 
+  board_ref: Ref
+
+  m_situation: Memo<MobileSituation>
+  _replay: Signal<Replay | undefined>
+  _path: Signal<string | undefined>
+
   constructor() {
 
+    let board_ref = Ref.make
+    this.board_ref = board_ref
+
+    let _path: Signal<string | undefined> = createSignal()
+    this._path = _path
+
+    let _replay: Signal<Replay | undefined> = createSignal(undefined, { equals: false })
+    this._replay = _replay
+
+    let m_moves = createMemo(() => {
+      let path = read(_path)
+      if (path) {
+        return read(_replay)?.follow_path(path).reduce((acc, node) => !acc ? node.uci : [acc, node.uci].join(' '), '') || ''
+      }
+      return ''
+    })
+
+    let m_situation = createMemo(() =>
+      playMoves(MobileSituation.from_fen(initial_fen), m_moves().split(' '))
+    )
+    this.m_situation = m_situation
+
+    const on_wheel = (dir: number) => {
+      if (dir < 0) {
+        owrite(_path, _ => !_ || _.length === 2 ? undefined : _.slice(0, -2))
+      } else {
+        owrite(_path, _ => {
+          let replay = read(_replay)
+          if (replay) {
+            if (!_) {
+              return replay.root.path
+            }
+            let extra =  replay.find_path(_)?.children[0]?.path
+            if (extra) {
+              return _ + extra
+            } else {
+              return _
+            }
+          } else {
+            return undefined
+          }
+        })
+      }
+    }
+
+    make_wheel_from_ref({ on_wheel }, board_ref)
   }
 
 }
--- src/idea_replay_view.tsx
+++ src/idea_replay_view.tsx
@@ -1,3 +1,4 @@
+import { Show } from 'solid-js'
 import { Chessboard23 } from 'chessboard23'
 import Chessreplay23 from 'chessreplay23'
 import { onScrollHandlers, set_$ref } from 'solid-play'
@@ -11,9 +12,11 @@ export default function (props: {}) {
 
   return (<>
      <div class='chessideareplay23'>
+       <div ref={set_$ref(ctrl.board_ref)} class='board-move-wrap'>
        <Chessboardmove fen={ctrl.fen} on_move={_ => { ctrl.on_move(_) }} allowed_ods={ctrl.allowed_ods}/>
+       </div>
        <div class='replay-wrap'>
-        <Chessreplay23 moves={ctrl.replay_moves}/>
+         <Chessreplay23 on_path={ctrl.path || ''} on_click={_ => ctrl.on_click(_)} moves={ctrl.replay_moves}/>
        </div>
      </div>
    </>)
